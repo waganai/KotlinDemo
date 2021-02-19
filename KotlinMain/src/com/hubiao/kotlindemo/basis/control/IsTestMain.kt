c233fe4b73467c6ab4584623dd5326275bc2d5f8
waganai <358973601@qq.com>
2021-02-19 14:42:35 +0800

    [功能][面向对象编程]is

--- KotlinMain/src/com/hubiao/kotlindemo/basis/control/IsTestMain.kt
+++ KotlinMain/src/com/hubiao/kotlindemo/basis/control/IsTestMain.kt
@@ -4,8 +4,9 @@ class IsTestMain {
 }
 
 fun main() {
-    isFunction1(1)
-    isFunction2(2F)
+//    isFunction1(1)
+//    isFunction2(2F)
+    isTestFunction()
 }
 
 fun isFunction1(any: Any) {
@@ -38,3 +39,29 @@ fun isFunction2(any: Any) {
         println("any is not Int")
     }
 }
+
+fun isTestFunction() {
+    val test0 = IsTestClass1()
+    val test1 = IsTestClass1Child1()
+    val test2 = IsTestClass1Child2()
+
+    println("test0 is IsTestClass1Child1 = ${test0 is IsTestClass1Child1}")
+    println("test0 is IsTestClass1Child2 = ${test0 is IsTestClass1Child2}")
+
+    println("test1 is IsTestClass1 = ${test1 is IsTestClass1}")
+    println("test1 is IsTestClass1Child1 = ${test1 is IsTestClass1Child1}")
+//    println("test1 is IsTestClass1Child2 = ${test1 is IsTestClass1Child2}")
+
+    println("test2 is IsTestClass1 = ${test2 is IsTestClass1}")
+//    println("test2 is IsTestClass1Child1 = ${test2 is IsTestClass1Child1}")
+    println("test2 is IsTestClass1Child2 = ${test2 is IsTestClass1Child2}")
+}
+
+open class IsTestClass1 {
+    var name = ""
+    var id = 0
+}
+
+class IsTestClass1Child1:IsTestClass1() {}
+class IsTestClass1Child2:IsTestClass1() {}
+
